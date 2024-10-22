893dbfa6bd5906fa386af99f0479fb8b455b9d02
unknown <chavezvalentin414@gmail.com>
2024-10-22 16:12:32 -0300

    commit con ignore

---
+++ Transaccion.java
@@ -0,0 +1,35 @@
+package SistemaBanco;
+
+import java.util.Date;
+
+public class Transaccion {
+	private Date fecha;
+	private float monto;
+	private int nroCliente;
+	
+	public Date getFecha() {
+		return fecha;
+	}
+	public void setFecha(Date fecha) {
+		this.fecha = fecha;
+	}
+	public float getMonto() {
+		return monto;
+	}
+	public void setMonto(float monto) {
+		this.monto = monto;
+	}
+	public int getNroCliente() {
+		return nroCliente;
+	}
+	public void setNroCliente(int nroCliente) {
+		this.nroCliente = nroCliente;
+	}
+	public Transaccion(Date fecha, float monto, int nroCliente) {
+		super();
+		this.fecha = fecha;
+		this.monto = monto;
+		this.nroCliente = nroCliente;
+	}
+	
+}
